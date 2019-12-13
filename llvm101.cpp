37ce45f29a49fba4223808a42d4cd770ed49646d
abner <yuxing.yuan@zilliz.com>
2019-12-13 15:47:15 +0800

    abner's solution

--- llvm101.cpp
+++ llvm101.cpp
@@ -68,6 +68,27 @@ class Engine {
     std::unique_ptr<ExecutionEngine> EE;
 };
 
+
+llvm::Value*
+do_operator(IRBuilder<> &builder, Op op, llvm::Value* l, llvm::Value * r) {
+
+    switch (op){
+        case Op::Plus:
+            return builder.CreateAdd(l, r);
+        case Op::Divide:
+            return builder.CreateSDiv(l, r);
+        case Op::Multiply:
+            return builder.CreateMul(l, r);
+        case Op::Minus :
+            return builder.CreateSub(l, r);
+        case Op::Modular :
+            return builder.CreateSRem(l, r);
+        default:
+            return nullptr;
+    }
+
+}
+
 // excise now!
 func_t
 Engine::get_function(Op op1, Op op2) {
@@ -76,6 +97,78 @@ Engine::get_function(Op op1, Op op2) {
 
     // TODO: insert code for funcion here!!
     // TODO: notice that builder, ctx, M is ready at "this" pointer
+
+    FunctionType* func_type = FunctionType::get(
+        Type::getVoidTy(ctx),
+        {Type::getInt32Ty(ctx), Type::getInt32PtrTy(ctx), Type::getInt32PtrTy(ctx), Type::getInt32PtrTy(ctx), Type::getInt32PtrTy(ctx)},
+        false
+        );
+
+
+    Function* foo = [&] () {
+
+        Function* function = Function::Create(func_type, Function::ExternalLinkage, func_name, mod);
+        BasicBlock * init_b = BasicBlock::Create(ctx, "InitBlock", function);
+        BasicBlock * if_b = BasicBlock::Create(ctx, "IfBranch", function);
+        BasicBlock * then_b = BasicBlock::Create(ctx, "ThenBranch", function);
+        BasicBlock * post_b = BasicBlock::Create(ctx, "PostBranch", function);
+
+        builder.SetInsertPoint(init_b);
+
+        // get all input parameters
+        auto iter = function->arg_begin();
+        Argument * size = iter++;
+        size->setName("size");
+
+        Argument *input_a = iter++;
+        input_a->setName("a");
+        Argument *input_b = iter++;
+        input_b->setName("b");
+        Argument *input_c = iter++;
+        input_c->setName("c");
+        Argument *input_d = iter++;
+        input_d->setName("d");
+
+        assert(iter == function->arg_end());
+
+        // init i
+        auto i = builder.CreateAlloca(Type::getInt32Ty(ctx), nullptr, "i");
+        builder.CreateStore(builder.getInt32(0), i);
+        builder.CreateBr(if_b);
+
+        // condition branch
+        builder.SetInsertPoint(if_b);
+        auto now_i = builder.CreateLoad(i, "now_i");
+        auto if_cond = builder.CreateICmpSLT(now_i, size, "i_slt_size");
+        builder.CreateCondBr(if_cond, then_b, post_b);
+
+        // then branch
+        builder.SetInsertPoint(then_b);
+        auto tmp_a_ptr = builder.CreateGEP(input_a, now_i, "tmp_a_ptr");
+        auto tmp_b_ptr = builder.CreateGEP(input_b, now_i, "tmp_b_ptr");
+        auto tmp_c_ptr = builder.CreateGEP(input_c, now_i, "tmp_c_ptr");
+        auto tmp_d_ptr = builder.CreateGEP(input_d, now_i, "tmp_d_ptr");
+
+        auto tmp_a = builder.CreateLoad(tmp_a_ptr, "tmp_a");
+        auto tmp_b = builder.CreateLoad(tmp_b_ptr, "tmp_b");
+        auto tmp_c = builder.CreateLoad(tmp_c_ptr, "tmp_c");
+
+        auto a_op1_b = do_operator(builder, op1, tmp_a,tmp_b);
+        a_op1_b->setName("a_op1_b");
+        auto res =do_operator(builder, op2, a_op1_b, tmp_c);
+        res->setName("tmp_result");
+        builder.CreateStore(res, tmp_d_ptr);
+
+        builder.CreateStore(builder.CreateAdd(now_i, builder.getInt32(1), "next_i"), i);
+        builder.CreateBr(if_b);
+
+        // post branch
+        builder.SetInsertPoint(post_b);
+        builder.CreateRet(nullptr);
+
+        return function;
+    }();
+
 
     EE = std::unique_ptr<ExecutionEngine>(EngineBuilder(std::move(Owner)).create());
     outs() << *mod;
@@ -86,7 +179,7 @@ Engine::get_function(Op op1, Op op2) {
 int
 main() {
     LLVM_Environment llvm_environment;
-    int size = 1000;
+    int size = 10000000;
     vector<int> vec_a = generate_vec(size);
     vector<int> vec_b = generate_vec(size);
     vector<int> vec_c = generate_vec(size);
@@ -95,7 +188,7 @@ main() {
 
     vector<int> vec_d(size);
     func_multiply_plus(size, vec_a.data(), vec_b.data(), vec_c.data(), vec_d.data());
-    //
+
     for (int i = 0; i < size; ++i) {
         auto ans = vec_d[i];
         auto ref = vec_a[i] * vec_b[i] + vec_c[i];
